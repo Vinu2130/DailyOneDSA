bd7551c5464535b579ebacfe58356fb3501587ff
vinuthna <panchagnulavinuthna02@gmail.com>
2025-07-05 16:20:03 +0530

    updated output format and corrected conditional logic in output

--- Day1_LinearSearch.py
+++ Day1_LinearSearch.py
@@ -6,14 +6,15 @@ def LinearSearch(arr,target):
     for i in range(len(arr)):
         if arr[i] == target:
             return i
+    return -1
 
 
 arr = [1,3,5,2,4,8,10,9]
 target = int(input("Enter the search element: "))
 target_position = LinearSearch(arr,target)
 
-if target_position:
-    print(f"The search element {target} is found at position : {target_position + 1}")
+if target_position >= 0:
+    print(f"The search element {target} is found at position : {target_position + 1} (i.e, at index {target_position})")
 else:
     print(f"The search element is not present in the available data")
 
