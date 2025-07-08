da47ba4b362fdc865ddf13f4c24c4baad02af756
vinuthna <panchagnulavinuthna02@gmail.com>
2025-07-08 22:35:21 +0530

    Day 4 : Selection Sort Implementation

---
+++ Day5_SelectionSort.py
@@ -0,0 +1,20 @@
+# Date : 08/07/2025 
+# Problem : Selection sort 
+# complexity : O(n^2)
+
+def SelectionSort(arr):
+    for i in range(len(arr)-1):
+        currentMinIndex = i
+        for j in range(i+1,len(arr)):
+            if (arr[j] < arr[currentMinIndex]):
+                currentMinIndex = j
+        if currentMinIndex != i:
+            arr[currentMinIndex],arr[i] = arr[i],arr[currentMinIndex]
+        print(arr)
+    return arr
+
+arr = [1,3,5,2,4,8,10,9]
+
+print(f"Array elements before sorting : {arr}")
+sortedArr = SelectionSort(arr)
+print(f"Array elements after sorting : {sortedArr}")
