e010e4d0fcd2f6c8ebd5eb4cf17374952f8e7069
vinuthna <panchagnulavinuthna02@gmail.com>
2025-07-14 16:51:43 +0530

    add search method to singly linked list to locate nodes by value

--- linked_lists/Day9_SinglyLinkedList.py
+++ linked_lists/Day9_SinglyLinkedList.py
@@ -10,12 +10,23 @@ class Node:
 class SinglyLinkedList:
     def __init__(self,head = None):
         self.head = head
-    def traversal(self, node : Node):
+    def traversal(self, node = None):
+        node = node or self.head
         current = node 
         while current: 
             print(f"{current.data} -> ", end = "")
             current = current.next
         print("None")
+    def searchElement(self):
+        k = int(input("Enter Search element : "))
+        current = self.head
+        while current:
+            if current.data == k:
+                print(f"Search Element {k} found in given singly linked list")
+                return
+            current = current.next
+        print(f"Search Element {k} is not present in given singly linked list")
+        
 
 # create single linked list : 0->1->2->3->4
 head = Node(0)
@@ -27,5 +38,7 @@ for i in range(1,5):
 
 
 sll = SinglyLinkedList(head)
-sll.traversal(head)
+sll.traversal()
+# sll.searchElement(3)
+sll.searchElement()
     
