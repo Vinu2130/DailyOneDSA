fccfc03c3aa3f297aba7c5167548d0d4b9863767
vinuthna <panchagnulavinuthna02@gmail.com>
2025-07-15 09:04:31 +0530

    Implemented Insertion operations

--- linked_lists/Day9_SinglyLinkedList.py
+++ linked_lists/Day9_SinglyLinkedList.py
@@ -10,9 +10,12 @@ class Node:
 class SinglyLinkedList:
     def __init__(self,head = None):
         self.head = head
+        
+    # Traversal Operations
     def traversal(self, node = None):
         node = node or self.head
         current = node 
+        print("The given Singly linked list is: ")
         while current: 
             print(f"{current.data} -> ", end = "")
             current = current.next
@@ -26,6 +29,62 @@ class SinglyLinkedList:
                 return
             current = current.next
         print(f"Search Element {k} is not present in given singly linked list")
+    def countElements(self):
+        count = 0
+        current  = self.head
+        # print("The given Singly linked list is: ")
+        while current: 
+            # print(f"{current.data} -> ", end = "")
+            count += 1
+            current = current.next
+        # print("None")
+        # print(f"Total Number of elements in given singly linked list : {count}")
+        return count
+        
+    # Insertion Operations
+    def insertAtBeginning(self,k):
+        newNode = Node(k)
+        newNode.next = self.head
+        self.head = newNode
+        return self.head
+    
+    def insertAtEnd(self,k):
+        newNode = Node(k)
+        if self.head == None:
+            self.head = newNode
+            return self.head
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = newNode
+        return self.head
+    
+    def insertAtPostion(self,k,pos):
+        newNode = Node(k)
+        noOfElements = self.countElements() 
+   
+        if pos==1 :
+            self.insertAtBeginning(k)
+            return
+       
+        elif pos > noOfElements:
+            self.insertAtEnd(k)
+            return
+        count = 2
+        temp = self.head
+        while count < pos:
+            print(temp.data, count)
+            count += 1
+            temp = temp.next
+            
+        newNode.next = temp.next
+        temp.next = newNode
+        return self.head
+    
+    
+        
+      
+        
         
 
 # create single linked list : 0->1->2->3->4
@@ -38,7 +97,16 @@ for i in range(1,5):
 
 
 sll = SinglyLinkedList(head)
-sll.traversal()
-# sll.searchElement(3)
-sll.searchElement()
+# sll = SinglyLinkedList()
+# sll.traversal()
+# # sll.searchElement(3)
+# sll.searchElement()
+# sll.countElements()
+sll.insertAtBeginning(5)
+sll.insertAtBeginning(6)
+sll.insertAtBeginning(7)
+sll.insertAtEnd(8)
+sll.insertAtPostion(10,10)
+# sll.traversal()
+# print(sll.countElements())
     
