2f3f6b4f3b216e409bf6157f52f249b13c4f9367
vinuthna <panchagnulavinuthna02@gmail.com>
2025-07-20 19:10:13 +0530

    Day 11 : Circular Linked List

--- linked_lists/Day11_CircularLinkedList.py
+++ linked_lists/Day11_CircularLinkedList.py
@@ -96,9 +96,63 @@ class CircularLinkedList:
         newNode.next = temp.next
         temp.next = newNode
         
+    # delete operations
+    def deleteAtBeginning(self):
+        # newNode = Node(k)
+        if self.first == None:
+            return f"There is no element present in given linkedlist"
+        deletedNode = self.first
+        self.first = self.first.next
+        self.last.next = self.first
+        deletedNode.next = None
+      
+        return deletedNode.data
+        
+    def deleteAtEnd(self):
+        # newNode = Node(k)
+        if self.first == None:
+            return f"There is no element present in given linkedlist"
+        elif self.first.next == self.first:
+            deletedNode = self.first
+            self.first = None
+            return deletedNode.data
+        temp = self.first
+        while temp.next.next != self.first:
+            temp = temp.next
+        deletedNode = temp.next
+        temp.next = self.first
+        return deletedNode.data
     
-            
-            
+    def deleteAtPosition(self, pos):
+        noOfElements = self.countElements() 
+        if noOfElements == 0 or pos > noOfElements or pos <= 0:
+            return f"None"    
+        if pos==1 :
+            return self.deleteAtBeginning()
+        elif pos == noOfElements:
+            return self.deleteAtEnd()
+        count = 2
+        temp = self.first
+        while count < pos:
+            # print(temp.data, count)
+            count += 1
+            temp = temp.next
+        deletedNode = temp.next 
+        temp.next = temp.next.next
+       
+        return deletedNode.data
+    
+    def searchElement(self):
+        k = int(input("Enter Search element : "))
+        current = self.first
+        while current:
+            if current.data == k:
+                print(f"Search Element {k} found in given circular linked list")
+                return
+            current = current.next
+            if current == self.first:
+                print(f"Search Element {k} is not present in given circular linked list")
+                return
         
         
         
@@ -112,14 +166,21 @@ class CircularLinkedList:
 
 cll = CircularLinkedList()
 cll.insertIntoEmptyList(0)
-# cll.insertAtBeginning(1)
-# cll.insertAtBeginning(2)
-# cll.insertAtBeginning(3)
-# cll.insertAtBeginning(9)
+cll.insertAtBeginning(1)
+cll.insertAtBeginning(2)
+cll.insertAtBeginning(3)
+cll.insertAtBeginning(9)
 cll.insertAtBeginning(10)
 cll.insertAtEnd(7)
 cll.insertAtPostion(5,3)
 print(cll.first.data)
 cll.traversal()
+print(cll.deleteAtBeginning())
+cll.traversal()
+print(cll.deleteAtEnd())
+cll.traversal()
+print(cll.deleteAtPosition(2))
+cll.traversal()
+cll.searchElement()
 
     
