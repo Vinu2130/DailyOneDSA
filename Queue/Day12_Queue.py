47e3320c05772ddc32635f18083b1c90b085c893
vinuthna <panchagnulavinuthna02@gmail.com>
2025-07-21 22:38:30 +0530

    Day12

---
+++ Queue/Day12_Queue.py
@@ -0,0 +1,7 @@
+# Date : 21/07/2025 
+# Problem : Queue
+
+'''
+Queue is a linear data structure that follows FIFO (First In First Out) Principle, so the first element inserted is the first to be popped out.
+'''
+
